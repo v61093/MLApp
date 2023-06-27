7fa5935797c520df72062649155de2634da50e36
v61093 <64950322+v61093@users.noreply.github.com>
2023-06-27 16:22:00 +0000

    Added dynamic data and API route

--- main.py
+++ main.py
@@ -1,10 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+# making changes to HTML doc can be trick
+# job data is stored some where else in a database
+# what is done is    1. information is fetch from a database
+#                    2. entered in to html file and then displayed on screen
+
+JOBS = [{
+  'id': 1,
+  'title': 'Data Analyst',
+  'location': "Banglore India",
+  'salary': 'Rs 10,00,000'
+}, {
+  'id': 2,
+  'title': 'Data Scientist',
+  'location': "Delhi India",
+  'salary': 'Rs 15,00,000'
+}, {
+  'id': 3,
+  'title': 'Frontend Engineer',
+  'location': "Remote",
+  'salary': 'Rs 12,00,000'
+}, {
+  'id': 4,
+  'title': 'Backend Engineer',
+  'location': "San Fransisco USA",
+  'salary': '$120,000'
+}]
+
+
 @app.route('/')
 def hello():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, company_name='VISH')
 
-if __name__=='__main__':
-  app.run(host = '0.0.0.0',debug = True)
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
